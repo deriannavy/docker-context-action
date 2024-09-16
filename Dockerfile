a220457238b68504eae1235f61fad2b057f24405
Derian Escalante <deriannavy@gmail.com>
2024-09-16 12:53:14 -0600

    Automatic Update 2024-09-16_12:53:14

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM docker:latest
+
+RUN apk --no-cache add openssh-client
+# docker-compose
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+set -eu
+
+if [ -z "$INPUT_REMOTE_DOCKER_HOST" ]; then
+    echo "Input remote_docker_host is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_SSH_PUBLIC_KEY" ]; then
+    echo "Input ssh_public_key is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_SSH_PRIVATE_KEY" ]; then
+    echo "Input ssh_private_key is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_ARGS" ]; then
+  echo "Input input_args is required!"
+  exit 1
+fi
+
+if [ -z "$INPUT_STACK_FILE_NAME" ]; then
+  INPUT_STACK_FILE_NAME=docker-compose.yml
+fi
+
+if [ -z "$INPUT_SSH_PORT" ]; then
+  INPUT_SSH_PORT=22
+fi
+
+STACK_FILE=${INPUT_STACK_FILE_NAME}
+DEPLOYMENT_COMMAND_OPTIONS="--host ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
+
+DEPLOYMENT_COMMAND="docker-compose -f $STACK_FILE"
+
+
+SSH_HOST=${INPUT_REMOTE_DOCKER_HOST#*@}
+
+echo "Registering SSH keys..."
+
+# register the private key with the agent.
+mkdir -p ~/.ssh
+ls ~/.ssh
+printf '%s\n' "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+printf '%s\n' "$INPUT_SSH_PUBLIC_KEY" > ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa.pub
+#chmod 600 "~/.ssh"
+eval $(ssh-agent)
+ssh-add ~/.ssh/id_rsa
+
+
+echo "Add known hosts"
+ssh-keyscan -p $INPUT_SSH_PORT "$SSH_HOST" >> ~/.ssh/known_hosts
+ssh-keyscan -p $INPUT_SSH_PORT "$SSH_HOST" >> /etc/ssh/ssh_known_hosts
+# set context
+echo "Create docker context"
+docker context create staging --docker "host=ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
+docker context use staging
+
+
+if  [ -n "$INPUT_DOCKER_LOGIN_PASSWORD" ] || [ -n "$INPUT_DOCKER_LOGIN_USER" ] || [ -n "$INPUT_DOCKER_LOGIN_REGISTRY" ]; then
+  echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: docker login"
+  docker login -u "$INPUT_DOCKER_LOGIN_USER" -p "$INPUT_DOCKER_LOGIN_PASSWORD" "$INPUT_DOCKER_LOGIN_REGISTRY"
+fi
+
+echo "Command: ${DEPLOYMENT_COMMAND} pull"
+${DEPLOYMENT_COMMAND} ${DEPLOYMENT_COMMAND_OPTIONS} pull
+
+echo "Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
+${DEPLOYMENT_COMMAND} ${DEPLOYMENT_COMMAND_OPTIONS} ${INPUT_ARGS}
+
+
